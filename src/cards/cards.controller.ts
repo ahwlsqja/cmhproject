4fa9fb546f2f3edbbed901d550427ff3a96f7b7f
8860k <ysmkk@naver.com>
2024-03-20 11:23:12 +0900

    작업자할당/변경 service

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -6,37 +6,73 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from "@nestjs/common"
 import { CardsService } from "./cards.service";
 import { CreateCardDto } from "./dto/create-card.dto";
 import { UpdateCardDto } from "./dto/update-card.dto";
+import { AuthGuard } from "@nestjs/passport";
+import { Users } from "src/users/entities/user.entity";       
 
+@UseGuards(AuthGuard('jwt'))
 @Controller("cards")
 export class CardsController {
   constructor(private readonly cardsService: CardsService) {}
 
-  @Post()
-  create(@Body() createCardDto: CreateCardDto) {
-    return this.cardsService.create(createCardDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.cardsService.findAll();
-  }
-
+  
+  // 카드 상세 조회
   @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.cardsService.findOne(+id);
+  async findCard(@Param("id") id: string) {
+    return this.cardsService.getCardsByColumnId(+id);
   }
 
+  // 카드 생성
+  @Post(':cardId')
+  async createCard(
+    @Param('cardId') cardId: number,
+    @Body() CreateCardDto: CreateCardDto,
+  ) {
+    await this.cardsService.createCard(
+      Users.id,
+      CreateCardDto.content,
+    );
+  }
+
+  // 카드 수정 / 담당자 변경
   @Patch(":id")
-  update(@Param("id") id: string, @Body() updateCardDto: UpdateCardDto) {
-    return this.cardsService.update(+id, updateCardDto);
+  async updateCard(
+    @Param('id') id: number,
+    @Body() UpdateCardDto: UpdateCardDto,
+  ) {
+    await this.cardsService.updateCard(
+      id,
+      UpdateCardDto.title,
+      UpdateCardDto.content,
+    );
   }
 
+  // 카드 삭제
   @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.cardsService.remove(+id);
+  async deleteCard( 
+    @Param('id') id: number) {
+    await this.cardsService.deleteCard(id, Users.id);
   }
+
+  // 작업자 할당
+
+
+
+  // 작업자 변경
+
+
+
+  // 칼럼 내 위치 변경
+
+
+
 }
+
+function UserInfo(): (target: CardsController, propertyKey: "createCard", parameterIndex: 0) => void {
+  throw new Error("Function not implemented.");
+}
+
